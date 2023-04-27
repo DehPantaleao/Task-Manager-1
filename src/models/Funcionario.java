a224549c507b3022c166308d24277d6b5f7238c3
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-27 15:02:51 -0300

    Adicionando esqueleto do menu, metodo de mostrarMenu criado e implantado

---
+++ src/models/Funcionario.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Funcionario {
+
+	private String nomeFuncionario;
+	private String cargoFuncionario;
+
+	public Funcionario(String nomeFuncionario, String cargoFuncionario) {
+		this.nomeFuncionario = nomeFuncionario;
+		this.cargoFuncionario = cargoFuncionario;
+	}
+
+	public String getNomeFuncionario() {
+		return nomeFuncionario;
+	}
+
+	public void setNomeFuncionario(String nomeFuncionario) {
+		this.nomeFuncionario = nomeFuncionario;
+	}
+
+	public String getCargoFuncionario() {
+		return cargoFuncionario;
+	}
+
+}
---
+++ src/utilities/Metodos.java
@@ -0,0 +1,35 @@
+package utilities;
+
+public class Metodos {
+	
+	// Metodo para mostrar o menu na classe Main
+	public static void mostrarMenu() {
+		System.out.println("""
+		---------------------------------------------
+				
+				Escolha uma Opção		 
+				           
+		---------------------------------------------
+			1 - Registrar um novo empregado			
+			2 - Listar os empregados registrados	
+			3 - Criar uma nova tarefa				
+			4 - Listas todas as tarefas ativas		
+			5 - Editar uma tarefa existente			
+			6 - Excluir uma tarefa					
+			0 - Sair do programa					
+													
+		---------------------------------------------
+		""");
+	}
+	
+	// Metodo para cadastrar um novo funcionario
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
