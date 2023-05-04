e18046d1999bbcce7b861de420b59ccbbb9c3846
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-05-03 23:34:07 -0300

    menu modificado

--- src/utilities/Metodos.java
+++ src/utilities/Metodos.java
@@ -15,23 +15,23 @@ public class Metodos {
 
     public static void mostrarMenu() {
         System.out.println("""
-				                ______________________________________________
-				                |			 Escolha uma Opção			     |
-				                ______________________________________________
-				                |											 |
-				                |	1 - Registrar um novo empregado			 |
-				                |	2 - Listar os empregados registrados	 |
-				                |	3 - Criar uma nova tarefa				 |
-				                |	4 - Listas todas as tarefas ativas		 |
-				                |	5 - Editar uma tarefa existente			 |
-				                |	6 - Excluir uma tarefa					 |
-				                |	0 - Sair do programa					 |
-				                |											 |
-				                ______________________________________________
-				                |        Escolha uma opção listada           |
-				                |    (Digite e pressione o botão Enter):     |
-				                ______________________________________________
-				""");
+==============================================
+=             Escolha uma Opção              =
+==============================================
+=                                            =
+=    1 - Registrar um novo empregado         =
+=    2 - Listar os empregados registrados    =
+=    3 - Criar uma nova tarefa               =
+=    4 - Listas todas as tarefas ativas      =
+=    5 - Editar uma tarefa existente         =
+=    6 - Excluir uma tarefa                  =
+=    0 - Sair do programa                    =
+=                                            =
+==============================================
+= Escolha uma opção listada                  =
+=(Digite e pressione o botão Enter):         =
+==============================================
+""");
     }
 
     public static void registrarFuncionario() {
