f2cb210fecad0464cf62475b06ea0d6a14c90987
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-05-03 22:54:34 -0300

    layout fixed

--- src/models/Tarefas.java
+++ src/models/Tarefas.java
@@ -4,50 +4,53 @@ import java.time.LocalDate;
 
 public class Tarefas {
 
-	private String nomeTarefa;
-	private String funcionarioAtribuido;
-	private String descricaoTarefa;
-	private LocalDate dataTarefaCriada;
+    private String nomeTarefa;
+    private String funcionarioAtribuido;
+    private String descricaoTarefa;
+    private LocalDate dataTarefaCriada;
 
-	public Tarefas(String nomeTarefa, String funcionarioAtribuido, String descricaoTarefa,
-				   LocalDate dataTarefaCriada) {
-		this.nomeTarefa = nomeTarefa;
-		this.funcionarioAtribuido = funcionarioAtribuido;
-		this.descricaoTarefa = descricaoTarefa;
-		this.dataTarefaCriada = dataTarefaCriada;
-	}
+    public Tarefas(String nomeTarefa, String funcionarioAtribuido, String descricaoTarefa,
+                   LocalDate dataTarefaCriada) {
+        this.nomeTarefa = nomeTarefa;
+        this.funcionarioAtribuido = funcionarioAtribuido;
+        this.descricaoTarefa = descricaoTarefa;
+        this.dataTarefaCriada = dataTarefaCriada;
+    }
 
-	public String getNomeTarefa() {
-		return nomeTarefa;
-	}
+    public String getNomeTarefa() {
+        return nomeTarefa;
+    }
 
-	public void setNomeTarefa(String nomeTarefa) {
-		this.nomeTarefa = nomeTarefa;
-	}
+    public void setNomeTarefa(String nomeTarefa) {
+        this.nomeTarefa = nomeTarefa;
+    }
 
-	public String getFuncionarioAtribuido() {
-		return funcionarioAtribuido;
-	}
+    public String getFuncionarioAtribuido() {
+        return funcionarioAtribuido;
+    }
 
-	public void setFuncionarioAtribuido(Funcionario funcionarioAtribuido) {
-		this.funcionarioAtribuido = String.valueOf(funcionarioAtribuido);
-	}
+    public void setFuncionarioAtribuido(Funcionario funcionarioAtribuido) {
+        this.funcionarioAtribuido = String.valueOf(funcionarioAtribuido);
+    }
 
-	public String getDescricaoTarefa() {
-		return descricaoTarefa;
-	}
+    public String getDescricaoTarefa() {
+        return descricaoTarefa;
+    }
 
-	public void setDescricaoTarefa(String descricaoTarefa) {
-		this.descricaoTarefa = descricaoTarefa;
-	}
+    public void setDescricaoTarefa(String descricaoTarefa) {
+        this.descricaoTarefa = descricaoTarefa;
+    }
 
-	public LocalDate getDataTarefaCriada() {
-		return dataTarefaCriada;
-	}
+    public LocalDate getDataTarefaCriada() {
+        return dataTarefaCriada;
+    }
 
-	public void setDataTarefaCriada(LocalDate dataTarefaCriada) {
-		this.dataTarefaCriada = dataTarefaCriada;
-	}
+    public void setDataTarefaCriada(LocalDate dataTarefaCriada) {
+        this.dataTarefaCriada = dataTarefaCriada;
+    }
 
-
+    @Override
+    public String toString() {
+        return nomeTarefa;
+    }
 }
--- src/utilities/Metodos.java
+++ src/utilities/Metodos.java
@@ -16,28 +16,27 @@ public class Metodos {
 
     public static void mostrarMenu() {
         System.out.println("""
-                                ______________________________________________
-                                |			 Escolha uma Opção			     |
-                                ______________________________________________
-                                |											 |
-                                |	1 - Registrar um novo empregado			 |
-                                |	2 - Listar os empregados registrados	 |
-                                |	3 - Criar uma nova tarefa				 |
-                                |	4 - Listas todas as tarefas ativas		 |
-                                |	5 - Editar uma tarefa existente			 |
-                                |	6 - Excluir uma tarefa					 |
-                                |	0 - Sair do programa					 |
-                                |											 |
-                                ______________________________________________
-                                |        Escolha uma opção listada           |
-                                |    (Digite e pressione o botão Enter):     |
-                                ______________________________________________
-                """);
+                ==============================================
+                =              Escolha uma Opção             =
+                ==============================================
+                =                                            =
+                =   1 - Registrar um novo funcionário        =
+                =   2 - Listar os funcionário registrados    =
+                =   3 - Criar uma nova tarefa                =
+                =   4 - Listas todas as tarefas ativas       =
+                =   5 - Editar uma tarefa existente          =
+                =   6 - Excluir uma tarefa                   =
+                =   0 - Sair do programa                     =
+                =                                            =
+                ==============================================
+                =        Escolha uma opção listada           =
+                =    (Digite e pressione o botão Enter):     =
+                ==============================================
+                                """);
     }
 
 
     public static void registrarFuncionario() {
-        System.out.println("=====================================================");
         System.out.print("\nDigite o nome do funcionário: ");
         String nomeFuncionario = leitura.nextLine();
         System.out.print("Digite o cargo desse funcionário: ");
@@ -45,41 +44,35 @@ public class Metodos {
         Funcionario novoFuncionario = new Funcionario(nomeFuncionario, cargoFuncionario);
         funcionarios.add(novoFuncionario);
         System.out.println("Funcionário registrado com sucesso\n");
-        System.out.println("=====================================================\n");
     }
 
 
     public static void listarFuncionarios() {
 
         if (funcionarios.isEmpty()) {
-            System.out.println("=====================================================");
             System.out.print(
                     "\nNão foi encontrado nenhum funcinário cadastrado. Deseja cadastrar um novo funcionário? (S/N): ");
             String escolha = leitura.nextLine();
             if (escolha.equalsIgnoreCase("s")) {
                 registrarFuncionario();
             } else {
-                System.out.println("Retornando para o menu. ");
-                System.out.println("=====================================================\n");
+                System.out.println("Retornando para o menu...\n");
             }
 
         } else {
-            System.out.println("=====================================================");
-            System.out.println("\nFuncionários registrados: ");
+            System.out.println("\nFuncionários registrados: \n");
             for (Funcionario listaFuncionarios : funcionarios) {
                 System.out.println("Nome do funcionário: " + listaFuncionarios.getNomeFuncionario() + ", Cargo: "
                         + listaFuncionarios.getCargoFuncionario());
             }
-            System.out.println("\n=====================================================");
         }
-
+        System.out.println("\n");
     }
 
     public static void criarTarefas() {
-        System.out.println("Criando uma nova tarefa: ");
-        System.out.print("Digite o nome da tarefa: ");
+        System.out.print("\nDigite o nome da tarefa: ");
         String nomeTarefa = leitura.nextLine();
-        System.out.println("Digite o nome do funcionário atribuido a esta tarefa: ");
+        System.out.print("Digite o nome do funcionário atribuido a esta tarefa: ");
         String funcionarioAtribuido = leitura.nextLine();
 
         Funcionario funcionarioExiste = null;
@@ -92,16 +85,16 @@ public class Metodos {
         }
 
         if (funcionarioExiste == null) {
-            System.out.println("Funcionario não encontrado, deseja cadastrar um novo funcionário? (S/N)");
+            System.out.print("Funcionario não encontrado, deseja cadastrar um novo funcionário? (S/N): ");
             String escolha = leitura.nextLine();
             if (escolha.equalsIgnoreCase("s")) {
                 registrarFuncionario();
             } else {
-                System.out.println("A criação da tarefa foi cancelada!");
+                System.out.println("A criação da tarefa foi cancelada!\n");
                 return;
             }
         }
-        System.out.println("Descreva a tarefa atribuida: ");
+        System.out.print("Descreva a tarefa atribuida: ");
         String descricaoTarefa = leitura.nextLine();
 
         LocalDate dataTarefaCriada = LocalDate.now();
@@ -109,36 +102,33 @@ public class Metodos {
 
         tarefas.add(novaTarefa);
 
-        System.out.println("Tarefa criada com sucesso!");
+        System.out.println("Tarefa criada com sucesso!\n");
     }
 
     public static void listarTarefas() {
         if (tarefas.isEmpty()) {
-            System.out.println("=====================================================");
-            System.out.println("Não há tarefas registradas.");
-            System.out.println("=====================================================");
+            System.out.println("\nNão há tarefas registradas.");
+            System.out.println("Retornando para o menu...\n");
         } else {
-            System.out.println("=====================================================");
-            System.out.println("Tarefas ativas:");
+            System.out.println("\nTarefas ativas:\n");
             for (Tarefas listarTarefas : tarefas) {
                 System.out.println("Nome da tarefa: " + listarTarefas.getNomeTarefa() + ", Funcionario atribuido: "
                         + (listarTarefas.getFuncionarioAtribuido() == null ? "Nenhum funcionário atribuído" : listarTarefas.getFuncionarioAtribuido()) + ", descrição: "
                         + listarTarefas.getDescricaoTarefa() + ", data de criação: "
                         + listarTarefas.getDataTarefaCriada());
             }
-            System.out.println("=====================================================");
+            System.out.println("\n");
         }
     }
 
     public static void editarTarefas() {
 
         if (tarefas.isEmpty()) {
-            System.out.println("Não há tarefas criadas para editar!");
+            System.out.println("\nNão há tarefas criadas para editar!\n");
             return;
         }
 
-        System.out.println("Editando uma tarefa: ");
-        System.out.println("Digite o nome da tarefa a ser editada: ");
+        System.out.println("\nDigite o nome da tarefa a ser editada: ");
         String nomeTarefa = leitura.nextLine();
 
         Tarefas tarefasParaEditar = null;
@@ -151,41 +141,41 @@ public class Metodos {
         }
 
         if (tarefasParaEditar == null) {
-            System.out.println("Tarefa não encontrada!");
-            System.out.println("Deseja criar uma nova tarefa? (S/N)");
+            System.out.println("\nTarefa não encontrada!");
+            System.out.println("Deseja criar uma nova tarefa? (S/N): ");
             String escolha = leitura.nextLine();
 
             if (escolha.equalsIgnoreCase("s")) {
                 criarTarefas();
             } else {
-                System.out.println("Retornando para o menu...");
+                System.out.println("\nRetornando para o menu...\n");
             }
         } else {
-            System.out.println("Tarefa encontrada!");
-            System.out.println(tarefasParaEditar);
+            System.out.println("\nTarefa encontrada!\n");
 
             System.out.println("""
                     O que deseja editar?
                                        
                     1 - Renomear a tarefa
-                    2 - Alterar funcionario atribuido
-                    3 - Alterar a descricao
-                    0 - Voltar ao menu
+                    2 - Alterar funcionario atribuído
+                    3 - Alterar a descrição
+                    0 - Cancelar alteração (Voltar para o menu)
                     """);
 
+            System.out.print("\nESCOLHA: ");
             int escolha = leitura.nextInt();
             leitura.nextLine();
 
             switch (escolha) {
                 case 1:
-                    System.out.println("Digite um novo nome para a tarefa: " + tarefasParaEditar);
+                    System.out.print("\nDigite um novo nome para a tarefa " + tarefasParaEditar + ": ");
                     String novoNome = leitura.nextLine();
                     tarefasParaEditar.setNomeTarefa(novoNome);
-                    System.out.println("Tarefa editada com sucesso");
+                    System.out.println("\nTarefa editada com sucesso\n");
                     break;
 
                 case 2:
-                    System.out.println("Digite o nome do novo funcionario a ser atribuido para a tarefa: ");
+                    System.out.print("\nDigite o nome do novo funcionario a ser atribuido para a tarefa: ");
                     String novoFuncionario = leitura.nextLine();
 
                     Funcionario funcionarioExiste = null;
@@ -198,34 +188,34 @@ public class Metodos {
                     }
 
                     if (funcionarioExiste == null) {
-                        System.out.println("Funcionario nao encontrado! Deseja cadastrar um novo funcionario? (S/N): ");
+                        System.out.print("\nFuncionario nao encontrado! Deseja cadastrar um novo funcionario? (S/N): ");
                         String escolhaCadastrar = leitura.nextLine();
                         if (escolhaCadastrar.equalsIgnoreCase("s")) {
                             registrarFuncionario();
                             funcionarioExiste = funcionarios.get(funcionarios.size() - 1);
                         } else {
-                            System.out.println("A edicao da tarefa foi cancelada! ");
+                            System.out.println("\nA edicao da tarefa foi cancelada! \n");
                             return;
                         }
                     }
 
                     tarefasParaEditar.setFuncionarioAtribuido(funcionarioExiste);
-                    System.out.println("Novo funcionario atribuido com sucesso!");
+                    System.out.println("\nNovo funcionario atribuido com sucesso!\n");
                     break;
 
                 case 3:
-                    System.out.println("Digite uma nova descricao a tarefa: ");
+                    System.out.print("\nDigite uma nova descricao a tarefa: ");
                     String novaDescricao = leitura.nextLine();
                     tarefasParaEditar.setDescricaoTarefa(novaDescricao);
-                    System.out.println("Descricao da tarefa editada com sucesso!");
+                    System.out.println("\nDescricao da tarefa editada com sucesso!\n");
                     break;
 
                 case 0:
-                    System.out.println("Fechando a edicao! Retornando para o menu...");
+                    System.out.println("\nFechando a edicao! Retornando para o menu...\n");
                     break;
 
                 default:
-                    System.out.println("Opcao invalida! Tente novamente...");
+                    System.out.println("\nOpção invalida! Tente novamente.\n");
                     break;
 
             }
@@ -233,5 +223,44 @@ public class Metodos {
         }
     }
 
+    public static void excluirTarefas() {
 
+        if (tarefas.isEmpty()) {
+            System.out.println("\nNão há tarefas para serem excluídas!\n");
+            return;
+        }
+
+        System.out.print("\nInsira o nome da tarefa que deseja excluir: ");
+        String nomeTarefaDeletar = leitura.nextLine();
+
+        Tarefas tarefaExcluir = null;
+
+        for (Tarefas tarefa : tarefas) {
+            if (tarefa.getNomeTarefa().equalsIgnoreCase(nomeTarefaDeletar)) {
+                tarefaExcluir = tarefa;
+                break;
+            }
+        }
+
+        if (tarefaExcluir == null) {
+            System.out.print("\nTarefa não Encontrada! Deseja criar uma nova tarefa? (S/N): ");
+            String escolha = leitura.nextLine();
+            if (escolha.equalsIgnoreCase("s")) {
+                criarTarefas();
+            } else {
+                System.out.println("\nRetornando ao menu...\n");
+            }
+        } else {
+            System.out.println("\nTarefa encontrada!");
+            System.out.print("Deseja realmente excluir a tarefa? (S/N): ");
+            String confirmacao = leitura.nextLine();
+
+            if (confirmacao.equalsIgnoreCase("s")) {
+                tarefas.remove(tarefaExcluir);
+                System.out.println("\nA tarefa foi excluída com sucesso!\n");
+            } else {
+                System.out.println("Exclusão cancelada! Retornando ao menu...");
+            }
+        }
+    }
 }
